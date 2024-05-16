a6c803e096d2ee8dcd31153ea96f0341d829a537
FilipRosquist <filip.rosquist@gritacademy.se>
2024-05-16 08:57:55 +0200

    1

--- 10_Async_API/Filip_Ovning/Script/script.js
+++ 10_Async_API/Filip_Ovning/Script/script.js
@@ -1,25 +1,77 @@
-let dishes = [];
-
-function addDish(name) {
-    dishes.push(name);
-    console.log(name + " has been added to the list of dishes.");
+function writeName(myName){
+    console.log(myName);
+    return myName;
 }
 
-function countDishes() {
-    return dishes.length;
+writeName("Filip");
+
+console.log("--------------------------------");
+
+function calculateArea(width, height){
+    let area = width * height;
+    console.log(area);
+    return area; 
 }
 
-function showDishes() {
-    console.log("List of added dishes:");
-    for (let i = 0; i < dishes.length; i++) {
-        console.log((i + 1) + ". " + dishes[i]);
+calculateArea(5, 5)
+
+console.log("--------------------------------");
+
+function countSum(n1, n2){
+    let sum = n1 + n2;
+    console.log(sum);
+    return sum;
+}
+
+countSum(5, 5)
+
+console.log("--------------------------------");
+
+function CelToFar(celsius){
+    let fahrenheit = (celsius * 9/5) + 32;
+    return fahrenheit;
+}
+
+const celsius = 10;
+const fahrenheit = CelToFar(celsius);
+console.log(`${celsius}°C är lika med ${fahrenheit}°F`);
+
+console.log("--------------------------------");
+
+function countAreaOfCircle(radie){
+
+    let areaCircle = Math.PI * (radie * radie);
+    return areaCircle;
+}
+
+let radie = 2;
+let areaCircle = countAreaOfCircle(radie);
+console.log(`Arean på en cirkel med radien ${radie} är ${areaCircle}`);
+
+console.log("--------------------------------");
+
+function countSq(Sq1){
+    let Sq = Sq1 * Sq1;
+    console.log(Sq);
+    return Sq;
+}
+
+countSq(10)
+
+console.log("--------------------------------");
+
+function countCharactersWithoutSpaces(string) {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== ' ') {
+            count++;
+        }
     }
+    return count;
 }
 
-addDish("Kebab");
-addDish("Burger");
-addDish("Pasta");
+// Example usage
+const text = "Hello world!";
+const characterCount = countCharactersWithoutSpaces(text);
+console.log(`The number of characters in the string "${text}" without spaces is ${characterCount}`);
 
-console.log("Number of added dishes:", countDishes());
-
-showDishes();
